e45f19f6d125f09f9e0e12365c2503f7280e06e9
Andrew Kofink <andrew.kofink@scimedsolutions.com>
2013-07-19 21:59:09 -0400

    Add a loggable Logger wrapper

---
+++ lib/loggable.rb
@@ -0,0 +1,25 @@
+module Loggable
+  def logger
+    @logger ||= setup_logger
+  end
+
+  def setup_logger
+    filename = 'log/libstatus.log'
+    old_files_max = 1
+    max_file_size = 1024000
+
+    Logger.new filename, old_files_max, max_file_size
+  end
+
+  def log_info args
+    logger.info args
+  end
+
+  def log_error args
+    logger.error args
+  end
+
+  def log_fatal args
+    logger.fatal args
+  end
+end
